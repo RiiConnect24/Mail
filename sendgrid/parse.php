6ae062b70885b53ff8ce9bbbe8596cbcfda74afe
jsa <thejsa@users.noreply.github.com>
2017-10-09 01:13:06 +0100

    Revert to TYPEIMAGE

--- sendgrid/parse.php
+++ sendgrid/parse.php
@@ -45,7 +45,7 @@ foreach($attachmentInfo as $key => $info) {
   if(!$_FILES[$key]['name']) { error_log('sendgrid says there was an attachment - '.$key.' - but we couldn\'t find it in $_FILES'); continue; }
   if($_FILES[$key]['error']) { error_log('error processing attachment '.$key.': '.$_FILES[$key]['error']); continue; } // if an error / file doesn't exist, go to next attachment
   $body[] = [
-    'type' => 'image',
+    'type' => TYPEIMAGE,
     'encoding' => 'base64',
     'subtype' => 'jpeg; name='.$info['name'],
 //     'description' => $info['name'],
