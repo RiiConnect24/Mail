9237f10c3dd1a7f30ffbbeadadfba0ac63e5c66b
Larsen Vallecillo <larsen@Weborican.com>
2017-11-08 09:57:12 -0600

    Fixed exec time

--- php/account.php
+++ php/account.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('max_execution_time', 200);
+ini_set('max_execution_time', 30);
 if(!require('config/config.php')) {
     echo ("cd=610\n");
     echo ("msg=Configuration file not found.\n");
